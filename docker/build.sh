2fef311b18639d56b9e5688a8b907fe0358b6c2c
rocket049 <fuhuizn@163.com>
2019-02-06 18:18:30 +0800

    docker builder 2019年 02月 06日 星期三 18:18:30 CST

---
+++ docker/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+mkdir -p powerchatserver/pems
+cp ../server/server powerchatserver/powerchatserver
+cp ../server/config.json.remote powerchatserver/config.json
+cp ../server/pems/a-cert.pem powerchatserver/pems/
+cp ../server/pems/a-key.pem powerchatserver/pems/
+tar cvfz powerchatserver.tar.gz powerchatserver
+
+docker build -t powerchatserver:v$1 .
+ 
+rm -rf powerchatserver
+rm powerchatserver.tar.gz
