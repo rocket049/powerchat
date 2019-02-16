d8489dcf90f7996e29b0850955bb8ae03441f4e8
rocket049 <fuhuizn@163.com>
2019-02-16 08:30:06 +0800

    2019年 02月 16日 星期六 08:30:06 CST

--- client/myOpen_windows.go
+++ client/myOpen_windows.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"os"
-
-	"github.com/skratchdot/open-golang/open"
-)
-
-func myOpen(path1 string) error {
-	st, err := os.Stat(path1)
-	if err != nil {
-		return err
-	}
-	if st.IsDir() {
-		err = open.StartWith(path1, "explorer.exe")
-	} else {
-		err = open.Start(path1)
-	}
-	return err
+func init() {
+	osID = 1
 }
