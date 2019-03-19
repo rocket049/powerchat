a4200749aca23f3522820c06d1e7a73b8432bd3a
rocket049 <fuhuizn@163.com>
2019-03-19 10:15:32 +0800

    2019年 03月 19日 星期二 10:15:31 CST

--- client/libclient/myOpen_windows.go
+++ client/libclient/myOpen_windows.go
@@ -1,5 +1,20 @@
 package main
 
-func init() {
-	osID = 1
+func myOpen(path1 string) error {
+	if strings.HasPrefix(path1, "http://") {
+		cmd := exec.Command("cmd", "/C", "start", path1)
+		cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+		return cmd.Start()
+	}
+
+	st, err := os.Stat(path1)
+	if err != nil {
+		return err
+	}
+	if st.IsDir() {
+		err = open.StartWith(path1, "explorer.exe")
+	} else {
+		err = open.Start(path1)
+	}
+	return err
 }
