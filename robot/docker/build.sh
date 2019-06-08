0f83d8ef0a771010f6d9248f6d0507d8d6d1bf50
rocket049 <fuhuizn@163.com>
2019-06-08 10:09:54 +0800

    2019年 06月 08日 星期六 10:09:54 CST

--- robot/docker/build.sh
+++ robot/docker/build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-
-docker build -t powerchatrobot .
+rm robot.tar.gz robot-docker.zip
+cp ../robot .
+mkdir data
+echo "VOLUME " >data/readme.txt
+tar cvfz robot.tar.gz robot data config.json
+zip -r9 robot-docker.zip robot.tar.gz Dockerfile
+#docker build -t powerchatrobot .
 
